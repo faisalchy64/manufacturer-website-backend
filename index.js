7e8100fa14cde7e86e5ed8a27986f8941d38b608
Faisal Karim <faisalchy64@gmail.com>
2022-05-27 08:09:33 +0600

    delete a specific order item from database

--- index.js
+++ index.js
@@ -88,6 +88,17 @@ async function run() {
 
             res.send(result);
         });
+
+        // delete a specific order item from database
+
+        app.delete("/order/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+
+            const result = await ordersCollection.deleteOne(query);
+
+            res.send(result);
+        });
     } finally {
         // await client.close();
     }
